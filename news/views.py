add5809e8e91c834d6a7defaac85ebb14c709d0a
Abhishek <abhishekabhi333549@gmail.com>
2025-11-23 07:48:50 +0000

    feat: add API endpoint for articles

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Article 
+from .serializers import ArticleSerializer 
 
-# Create your views here.
+class ArticleList(generics.ListAPIView):
+    queryset = Article.objects.order_by()
+    serializer_class = ArticleSerializer
